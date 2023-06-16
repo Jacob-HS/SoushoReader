3f7eb8131a9dc36d0eb461081f2ee8ab6b417525
Jacob-HS <jacobsabbagh99@gmail.com>
2023-06-17 08:30:42 +0900

    frickboi

--- website/static/home.js
+++ website/static/home.js
@@ -1,7 +1,9 @@
-
-
+//<div class="tooltipAlert">!</div>
+//<div class="tooltipText">This kanji is the old form of 口</div>
 var socket = io();
 let cropper;
+let kyuujitaiDict = {'亞': '亜', '惡': '悪', '壓': '圧', '圍': '囲', '爲': '為', '醫': '医', '壹': '壱', '稻': '稲', '飮': '飲', '隱': '隠', '羽': '羽', '營': '営', '榮': '栄', '衞': '衛', '益': '益', '驛': '駅', '悅': '悦', '圓': '円', '艷': '艶', '鹽': '塩', '奧': '奥', '應': '応', '橫': '横', '歐': '欧', '毆': '殴', '穩': '穏', '假': '仮', '價': '価', '畫': '画', '會': '会', '囘': '回', '懷': '懐', '繪': '絵', '擴': '拡', '殼': '殻', '覺': '覚', '學': '学', '嶽': '岳', '樂': '楽', '勸': '勧', '卷': '巻', '寬': '寛', '歡': '歓', '罐': '缶', '觀': '観', '閒': '間', '關': '関', '陷': '陥', '館': '館', '巖': '巌', '顏': '顔', '歸': '帰', '氣': '気', '龜': '亀', '僞': '偽', '戲': '戯', '犧': '犠', '卻': '却', '糺': '糾', '舊': '旧', '據': '拠', '擧': '挙', '峽': '峡', '挾': '挟', '敎': '教', '狹': '狭', '鄕': '郷', '堯': '尭', '曉': '暁', '區': '区', '驅': '駆', '勳': '勲', '薰': '薫', '羣': '群', '徑': '径', '惠': '恵', '攜': '携', '溪': '渓', '經': '経', '繼': '継', '莖': '茎', '螢': '蛍', '輕': '軽', '鷄': '鶏', '藝': '芸', '缺': '欠', '儉': '倹', '劍': '剣', '圈': '圏', '檢': '検', '權': '権', '獻': '献', '縣': '県', '險': '険', '顯': '顕', '驗': '験', '嚴': '厳', '效': '効', '廣': '広', '恆': '恒', '鑛': '鉱', '號': '号', '國': '国', '黑': '黒', '濟': '済', '碎': '砕', '齋': '斎', '劑': '剤', '冱': '冴', '櫻': '桜', '册': '冊', '雜': '雑', '參': '参', '慘': '惨', '棧': '桟', '蠶': '蚕', '贊': '賛', '殘': '残', '絲': '糸', '飼': '飼', '齒': '歯', '兒': '児', '辭': '辞', '濕': '湿', '實': '実', '舍': '舎', '寫': '写', '釋': '釈', '壽': '寿', '收': '収', '從': '従', '澁': '渋', '獸': '獣', '縱': '縦', '肅': '粛', '處': '処', '緖': '緒', '諸': '諸', '敍': '叙', '奬': '奨', '將': '将', '牀': '床', '燒': '焼', '祥': '祥', '稱': '称', '證': '証', '乘': '乗', '剩': '剰', '壤': '壌', '孃': '嬢', '條': '条', '淨': '浄', '疊': '畳', '穰': '穣', '讓': '譲', '釀': '醸', '囑': '嘱', '觸': '触', '寢': '寝', '愼': '慎', '晉': '晋', '眞': '真', '神': '神', '刄': '刃', '盡': '尽', '圖': '図', '粹': '粋', '醉': '酔', '隨': '随', '髓': '髄', '數': '数', '樞': '枢', '瀨': '瀬', '晴': '晴', '淸': '清', '精': '精', '靑': '青', '聲': '声', '靜': '静', '齊': '斉', '蹟': '跡', '攝': '摂', '竊': '窃', '專': '専', '戰': '戦', '淺': '浅', '潛': '潜', '纖': '繊', '踐': '践', '錢': '銭', '禪': '禅', '曾': '曽', '雙': '双', '壯': '壮', '搜': '捜', '插': '挿', '爭': '争', '窗': '窓', '總': '総', '聰': '聡', '莊': '荘', '裝': '装', '騷': '騒', '增': '増', '臟': '臓', '藏': '蔵', '屬': '属', '續': '続', '墮': '堕', '體': '体', '對': '対', '帶': '帯', '滯': '滞', '臺': '台', '瀧': '滝', '擇': '択', '澤': '沢', '單': '単', '擔': '担', '膽': '胆', '團': '団', '彈': '弾', '斷': '断', '癡': '痴', '遲': '遅', '晝': '昼', '蟲': '虫', '鑄': '鋳', '猪': '猪', '廳': '庁', '聽': '聴', '敕': '勅', '鎭': '鎮', '塚': '塚', '遞': '逓', '鐵': '鉄', '轉': '転', '點': '点', '傳': '伝', '都': '都', '黨': '党', '盜': '盗', '燈': '灯', '當': '当', '鬪': '闘', '德': '徳', '獨': '独', '讀': '読', '屆': '届', '繩': '縄', '貳': '弐', '姙': '妊', '黏': '粘', '惱': '悩', '腦': '脳', '霸': '覇', '廢': '廃', '拜': '拝', '賣': '売', '麥': '麦', '發': '発', '髮': '髪', '拔': '抜', '飯': '飯', '蠻': '蛮', '祕': '秘', '濱': '浜', '甁': '瓶', '福': '福', '拂': '払', '佛': '仏', '竝': '並', '變': '変', '邊': '辺', '邉': '辺', '辨': '弁', '辯': '弁', '瓣': '弁', '舖': '舗', '穗': '穂', '寶': '宝', '萠': '萌', '襃': '褒', '豐': '豊', '沒': '没', '飜': '翻', '槇': '槙', '萬': '万', '滿': '満', '默': '黙', '餠': '餅', '彌': '弥', '藥': '薬', '譯': '訳', '藪': '薮', '豫': '予', '餘': '余', '與': '与', '譽': '誉', '搖': '揺', '樣': '様', '謠': '謡', '遙': '遥', '瑤': '瑶', '慾': '欲', '來': '来', '賴': '頼', '亂': '乱', '覽': '覧', '畧': '略', '隆': '隆', '龍': '竜', '兩': '両', '獵': '猟', '綠': '緑', '鄰': '隣', '凛': '凜', '壘': '塁', '勵': '励', '禮': '礼', '隸': '隷', '靈': '霊', '齡': '齢', '戀': '恋', '爐': '炉', '勞': '労', '朗': '朗', '樓': '楼', '郞': '郎', '祿': '禄', '亙': '亘', '灣': '湾'};
+let kyuujitaiKeys =Object.keys(kyuujitaiDict);
 let sampleImageData;
 let preloadedImages = [];
 let cropCount=0;
@@ -10,8 +12,6 @@ let currQuestion;
 let resizedImages = {};
 let originalImages = {};
 socket.on("answer", function(data){
-  console.log("recieved");
-  console.log(data);
   sampleImageData=data;
   totalQuestions=(Object.keys(data)).length;
   currQuestion=1;
@@ -25,6 +25,16 @@ document.getElementById("addCropButton").addEventListener("click", ()=>{
   cropCount++;
   let img = document.getElementById("finishedCrop"+(cropCount));
   document.getElementById("cropDeleteButton"+cropCount).classList.remove("hidden");
+  if (Object.keys(cropper.getCropBoxData()).length === 0){
+    let data={
+      left: 0,
+      right: 0,
+      width: cropper.getImageData().width,
+      height: cropper.getImageData().height
+    };
+    cropper.crop();
+    cropper.setCropBoxData(data);
+  }
   let sameSizeResult = cropper.getCroppedCanvas({fillColor: "white"});
   let resizedResult = cropper.getCroppedCanvas({fillColor: "white", width: 100, height: 100});
   let imgURL=sameSizeResult.toDataURL('image/jpeg');
@@ -104,11 +114,11 @@ document.getElementById("submitCropButton").addEventListener("click", () => {
   
 });
 
-document.getElementById("pred1").addEventListener("click",()=>switchActiveAnswer(currQuestion, 1));
-document.getElementById("pred2").addEventListener("click",()=>switchActiveAnswer(currQuestion, 2));
-document.getElementById("pred3").addEventListener("click",()=>switchActiveAnswer(currQuestion, 3));
-document.getElementById("pred4").addEventListener("click",()=>switchActiveAnswer(currQuestion, 4));
-document.getElementById("pred5").addEventListener("click",()=>switchActiveAnswer(currQuestion, 5));
+document.getElementById("predContainer1").addEventListener("click",()=>switchActiveAnswer(currQuestion, 1));
+document.getElementById("predContainer2").addEventListener("click",()=>switchActiveAnswer(currQuestion, 2));
+document.getElementById("predContainer3").addEventListener("click",()=>switchActiveAnswer(currQuestion, 3));
+document.getElementById("predContainer4").addEventListener("click",()=>switchActiveAnswer(currQuestion, 4));
+document.getElementById("predContainer5").addEventListener("click",()=>switchActiveAnswer(currQuestion, 5));
 
 document.getElementById("questionImageButton1").addEventListener("click", ()=>switchActiveQuestion(1));
 document.getElementById("questionImageButton2").addEventListener("click", ()=>switchActiveQuestion(2));
@@ -224,6 +234,7 @@ function cropImage(){
         viewMode: 2,
         movable: false,
         minContainerWidth: 0,
+        minContainerHeight: 200,
         highlight: false,
         ready(){
           cropper.setCanvasData({ height: img.clientHeight, width: img.clientWidth  });
@@ -244,6 +255,9 @@ function displayQuestionImage(){
   document.getElementById("dragAndDropLabel").style.display="none";
   document.getElementById("showImage").style.display="block";
   IMG_COUNT = Object.keys(resizedImages).length;
+  if(IMG_COUNT == 1){
+    return;
+  }
   for(let i=1;i<=IMG_COUNT;i++){
     document.getElementById("questionImageButton"+i).classList.remove("hidden", "activeQuestion");
   }
@@ -256,16 +270,48 @@ function removePredictionPlaceholder(){
 }
 
 function displayAnswers(answerList){
+  removeTooltips();
   let i=1;
   for (const answer of answerList){
     let temp=document.getElementById("pred"+i);
     temp.innerHTML=answer;
+    if (kyuujitaiKeys.includes(answer)){
+      addTooltip(i, answer);
+    }
     i++;
   }
   let predcontainers = document.getElementsByClassName("prediction");
   for(const element of predcontainers){
     element.style.animationPlayState="running";
     element.classList.remove("hidden");
+  }
+}
+
+function addTooltip (index, kanji){
+  let container = document.getElementById("predContainer"+index);
+  let newkanji = kyuujitaiDict[kanji];
+  let alertdiv = document.createElement("div");
+  let alerttxt = document.createTextNode("!");
+  alertdiv.appendChild(alerttxt);
+  let tooltip = document.createElement("div");
+  let tooltiptext = document.createTextNode("This kanji is the old form of "+newkanji);
+  tooltip.appendChild(tooltiptext);
+
+  alertdiv.classList.add("tooltipAlert");
+  tooltip.classList.add("tooltipText");
+
+  container.appendChild(alertdiv);
+  container.appendChild(tooltip);
+}
+
+function removeTooltips(){
+  let tooltipelements = (document.getElementsByClassName("tooltipAlert"));
+  for (const element of tooltipelements){
+    element.remove();
+  }
+  tooltipelements = document.getElementsByClassName("tooltipText");
+  for (const element of tooltipelements){
+    element.remove();
   }
 }
 
