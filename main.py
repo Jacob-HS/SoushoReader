674d6be0adb0622e122da08b044847774c7c2a20
Jacob-HS <jacobsabbagh99@gmail.com>
2023-06-22 21:03:01 +0900

    added things to test

--- main.py
+++ main.py
@@ -76,18 +76,19 @@ def assessCustomImage(image, numOfResponses=1):
 @socketio.event
 def askQuestion (data):
     print("fuck yeet")
-    global model
-    answerList={}
-    for idx in range(len(data)):
-      im = cv2.imdecode(np.array(data[str(idx+1)], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-      result=assessCustomImage(im, 5)
-      info={}
-      for kanji in result:
-        i='U+'+hex(ord(kanji)).upper()[2:]
-        info[kanji]=os.listdir('website\\static\\samples\\'+i)
-      answerList[str(idx+1)]=info
-
-    socketio.emit("answer", answerList)
+    os.listdir("testFolder")
+    #global model
+    #answerList={}
+    #for idx in range(len(data)):
+    #  im = cv2.imdecode(np.array(data[str(idx+1)], dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+    #  result=assessCustomImage(im, 5)
+    #  info={}
+    #  for kanji in result:
+    #    i='U+'+hex(ord(kanji)).upper()[2:]
+    #    info[kanji]=os.listdir('website\\static\\samples\\'+i)
+    #  answerList[str(idx+1)]=info
+#
+    #socketio.emit("answer", answerList)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
